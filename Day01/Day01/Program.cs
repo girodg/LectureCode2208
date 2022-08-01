9fbb53141dc3324cf2f877a8a0591d99897f68e9
Garrett Girod <girodg@hotmail.com>
2022-08-01 10:57:18 -0400

    Day01

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,7 +6,61 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            //string location = Console.ReadLine();
+            //Console.WriteLine($"Hello {location}!");
+
+            PrintMessage();
+            string msg = GetMessage();
+            PrintMessage(msg);
+
+            Player p1 = new Player();
+            int spacesToMove = 3;
+            p1.MoveRight(spacesToMove);
+            int xp = 0, yp = 0;
+            p1.GetPosition(ref xp, ref yp);
+            Console.WriteLine($"You are at: {xp},{yp}");
+
+            int x1 = 0, y1 = 0;
+            p1.GetPosition(ref x1, ref y1);
+            Console.WriteLine(DateTime.Now);
         }
 
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello Gotham!");
+        }
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static string GetMessage()
+        {
+            Console.Write("Please enter your location: ");
+            string message = Console.ReadLine();
+            return message;
+        }
+    }
+    class Player
+    {
+        int x, y;
+
+        public void MoveRight()
+        {
+            if (x < Console.WindowWidth - 1)
+                x++;
+        }
+
+        public void MoveRight(int spaces)//pass by value
+        {
+            if (x + spaces <= Console.WindowWidth - 1)
+                x += spaces;
+        }
+
+        public void GetPosition(ref int xpos, ref int ypos)//pass by reference
+        {
+            xpos = x;
+            ypos = y;
+        }
     }
 }
