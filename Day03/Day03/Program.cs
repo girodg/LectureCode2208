d75f47764b90ea3e908bd11bde38e5001f950d69
Garrett Girod <girodg@hotmail.com>
2022-08-05 11:10:31 -0400

    Drop and Curve student

--- Day03/Day03/Program.cs
+++ Day03/Day03/Program.cs
@@ -60,6 +60,29 @@ namespace Day03
                 //Key and Value properties on kvp
                 Console.WriteLine($"{kvp.Key}\tSpeed: {kvp.Value.Speed}");
             }
+            bool wasRemoved = monsters.Remove(ZombieClass.Baby);
+            if(!wasRemoved)
+                Console.WriteLine("Baby zombies not in the dictionary.");
+            else
+                Console.WriteLine("Baby zombies were removed.");
+
+
+            //2 ways to check
+            //1. ContainsKey
+            //2. TryGetValue
+
+            //1. ContainsKey
+            if (monsters.ContainsKey(ZombieClass.Baby))
+            {
+                Zombie baby = monsters[ZombieClass.Baby];
+                baby.Speed = 30;
+            }
+
+            //2. TryGetValue
+            if(monsters.TryGetValue(ZombieClass.Baby, out Zombie babyZ))
+            {
+                monsters[ZombieClass.Baby] = new Zombie() { Speed = 40 };//overwrite the existing value
+            }
 
             DictionaryChallenge();
 
@@ -77,7 +100,63 @@ namespace Day03
                 pg2[student] = rando.NextDouble() * 100;
             }
 
+            PrintGrades(pg2);
+            DropStudent(pg2);
+            CurveStudent(pg2);
+        }
 
+        static void CurveStudent(Dictionary<string, double> course)
+        {
+            do
+            {
+                Console.Write("Please enter the name of the student to curve: ");
+                string student = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(student)) break;
+
+                if (course.TryGetValue(student, out double grade))
+                    course[student] = (grade > 95) ? 100 : grade + 5;
+                else
+                    Console.WriteLine($"{student} was not in the course.");
+
+                PrintGrades(course);
+            } while (true);
+        }
+
+        static void DropStudent(Dictionary<string, double> course)
+        {
+            do
+            {
+                Console.Write("Please enter the name of the student to drop: ");
+                string student = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(student)) break;
+
+                bool wasRemoved = course.Remove(student);
+                if (wasRemoved)
+                    Console.WriteLine($"{student} was removed from the course.");
+                else
+                    Console.WriteLine($"{student} was not in the course.");
+                PrintGrades(course);
+            } while (true);
+        }
+
+        static void PrintGrades(Dictionary<string, double> course)
+        {
+            Console.WriteLine("---------GRADES-----------");
+            foreach (var student in course)
+            {
+                double grade = student.Value;
+                string name = student.Key;
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow :
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                          ConsoleColor.Green;
+                Console.Write($"{grade,8:N2}");
+                Console.ResetColor();
+                Console.WriteLine($" {name}");
+            }
         }
     }
 }
