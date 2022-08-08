797e3b3fff370ffb28477ef75f8d34fb67546929
Garrett Girod <girodg@hotmail.com>
2022-08-08 09:53:47 -0400

    writing CSV

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace Day04
 {
+    enum Powers
+    {
+        Speed, Flying, ShapeShifting, Money, Swimming, Strength
+    }
+
+    class Superhero
+    {
+        public string Name { get; set; }
+        public string SecretIdentity { get; set; }
+        public Powers Power { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Superhero best = new Superhero() { Name = "Batman", SecretIdentity = "Bruce Wayne", Power = Powers.Money };
+
+            string filePath = @"C:\temp\2208\Heroes.txt";
+            char delimiter = '>';
+            //1. open the file.
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                //2. write to the file
+                sw.Write(best.Name);
+                sw.Write(delimiter);
+                sw.Write(best.SecretIdentity);
+                sw.Write(delimiter);
+                sw.Write(best.Power);
+            }//3. CLOSE THE FILE!!
         }
     }
 }
