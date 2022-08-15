5957fe172c1997a7141fc50a00998ebf51a6c8d6
Garrett Girod <girodg@hotmail.com>
2022-08-15 09:57:40 -0400

    Classes

---
+++ Day07/Day07CL/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class GameObject
+    {
+        //ACCESS MODIFIERS:
+        //private: ONLY this class can see it
+        //public: EVERYONE can see it
+        //protected: this class and all my descendent classes can see it
+
+        //FIELDS: data of your object
+        //
+        private ConsoleColor _color;
+
+        void DrawMe()
+        {
+            ConsoleColor color;
+            _color = ConsoleColor.Green;
+        }
+
+    }
+}
